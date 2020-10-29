f9e7c1dd8d12c18535d40b48ca119e1f17339153
Martayla Poellinitz <mdp527@gmail.com>
2020-10-29 16:51:33 -0400

    message

---
+++ morningsaturday.rb
@@ -0,0 +1,17 @@
+require './SaturdayMorning.rb'
+
+a = 6
+spme_string = "this text"
+
+front_yard = Gardenbox.new 10,20
+frontyardarea = front_yard.area
+frontyardperimeter = frony_yard.perimeter
+puts front_yard @width # need to make the varaible using an att_accessor
+
+# front yard is an object
+#       there are 3 functions within it
+#           initialize
+#           area
+#           perimeter
+
+back_yard = Gardenbox.new 14, 72
